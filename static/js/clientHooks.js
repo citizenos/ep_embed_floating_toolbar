c29b89d87e3a0a73fad2018cd9154711003e32b4
ilmar <ilmar.tyrk@gmail.com>
2020-12-01 23:37:37 +0200

    add eslint

--- static/js/clientHooks.js
+++ static/js/clientHooks.js
@@ -1,5 +1,41 @@
 'use strict';
 
+/**
+ * Position toolbar based on the `ep_embed_floating_toolbar_
+ * scroll` postMessage and all popups relative to it.
+ *
+ * @param {object} $toolbar JQuery object representing the toolbar
+ * @param {object} $popups JQuery object representing the popup elements
+ */
+const doPosition = ($toolbar, $popups) => {
+  // Initial values
+  $toolbar.css('top', 0);
+  $toolbar.css('z-index', 1000);
+
+  $popups.css('top', $toolbar.outerHeight()); // All popups appear under the toolbar
+
+  $(window).on('message onmessage', (e) => {
+    const msg = e.originalEvent.data;
+    if (msg.name === 'ep_embed_floating_toolbar_scroll') {
+      console.log('ep_embed_floating_toolbar_scroll', 'msg received', msg);
+      const data = msg.data;
+
+      if (!data.scroll) {
+        console.warn('Dropping message, as data.scroll was not present');
+        return;
+      }
+
+      const diff = data.scroll.top - data.frameOffset.top;
+      if (diff > 0) {
+        $toolbar.css('top', `${diff}px`);
+        $popups.css('top', `${diff + $toolbar.outerHeight()}px !important`);
+      } else {
+        $toolbar.css('top', '0');
+        $popups.css('top', `${$toolbar.outerHeight()}px !important`);
+      }
+    }
+  });
+};
 
 /**
  * Etherpad Lite "documentReady" hook
@@ -8,12 +44,12 @@
  *
  * @see {@link http://etherpad.org/doc/v1.6.1/#index_documentready}
  */
-exports.documentReady = function () {
-    const $editbar = $('#editbar');
+exports.documentReady = () => {
+  const $editbar = $('#editbar');
 
-    const $popups = $('#users, .popup, .epEmbedFloatingToolbar');
+  const $popups = $('#users, .popup, .epEmbedFloatingToolbar');
 
-    doPosition($editbar, $popups);
+  doPosition($editbar, $popups);
 };
 
 /**
@@ -23,47 +59,11 @@ exports.documentReady = function () {
  *
  * @see {@link http://etherpad.org/doc/v1.6.1/#index_posttimesliderinit}
  */
-exports.postTimesliderInit = function () {
-    console.debug('postTimesliderInit', window.location.href);
+exports.postTimesliderInit = () => {
+  console.debug('postTimesliderInit', window.location.href);
 
-    const $timeslider = $('#timeslider-top');
-    const $popups = $('.popup .epEmbedFloatingToolbar');
+  const $timeslider = $('#timeslider-top');
+  const $popups = $('.popup .epEmbedFloatingToolbar');
 
-    doPosition($timeslider, $popups);
-};
-
-/**
- * Position toolbar based on the `ep_embed_floating_toolbar_scroll` postMessage and all popups relative to it.
- *
- * @param {object} $toolbar JQuery object representing the toolbar
- * @param {object} $popups JQuery object representing the popup elements
- */
-const doPosition = function ($toolbar, $popups) {
-    // Initial values
-    $toolbar.css('top', 0);
-    $toolbar.css('z-index', 1000);
-
-    $popups.css('top', $toolbar.outerHeight()); // All popups appear under the toolbar
-
-    $(window).on('message onmessage', function (e) {
-        const msg = e.originalEvent.data;
-        if (msg.name === 'ep_embed_floating_toolbar_scroll') {
-            console.log('ep_embed_floating_toolbar_scroll', 'msg received', msg);
-            const data = msg.data;
-
-            if (!data.scroll) {
-                console.warn('Dropping message, as data.scroll was not present');
-                return;
-            }
-
-            const diff = data.scroll.top - data.frameOffset.top;
-            if (diff > 0) {
-                $toolbar.css('top', diff + 'px');
-                $popups.css('top', diff + $toolbar.outerHeight() + 'px !important');
-            } else {
-                $toolbar.css('top', '0');
-                $popups.css('top', $toolbar.outerHeight() + 'px !important');
-            }
-        }
-    });
+  doPosition($timeslider, $popups);
 };
